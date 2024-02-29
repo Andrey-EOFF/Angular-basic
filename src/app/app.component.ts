b1fc5984fc47f8d94cd8dd5422d1d3dac77c0f92
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2024-02-29 15:34:14 +0200

    derective-pipe-ng--

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,54 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NewComponent } from "./components/new/new.component";
-import { New2Component } from "./components/new2/new2.component";
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+export interface Role {
+  role: string,
+  text: string
+}
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, NewComponent, New2Component]
+    styleUrl: './app.component.scss',
+    imports: [RouterModule, CommonModule]
 })
 export class AppComponent {
   title: string = 'Angular-basic';
+  isParagraphVisible: boolean = true;
+  list: string[] = [
+    'list one',
+    'list two',
+    'list three'
+  ];
+
+  roles: Role[] = [
+    {
+      role: 'admin',
+      text: 'admin'
+    },
+    {
+      role: 'user',
+      text: 'user'
+    },
+    {
+      role: 'customer',
+      text: 'Ivan'
+    },
+    {
+      role: 'user',
+      text: 'user2'
+    }
+  ];
+ 
+  show() {
+    console.log('isParagraphVisible', this.isParagraphVisible)
+  }
+  getColor(user: Role): string {
+    return user.role === 'admin' ? 'green' : user.role === 'user' ? 'blue' : 'red'
+  }
+  getDate() {
+    return new Date()
+  }
 }
