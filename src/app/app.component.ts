e16d25222a27e516ebe5a7ee7de04494bbac5ec3
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2024-02-29 13:11:51 +0200

    test-new/component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from "./components/new/new.component";
+import { New2Component } from "./components/new2/new2.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NewComponent, New2Component]
 })
 export class AppComponent {
   title: string = 'Angular-basic';
